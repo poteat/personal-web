7f17d26ce8a01e02028dd4cd2b34a981017a3722
poteat <>
2019-01-26 03:32:35 +0000

    website upload (Fri, Jan 25, 2019 10:32:35 PM)

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-rm -rf ../poteat.github.io/
-hugo -d ../poteat.github.io
+rm -rf poteat.github.io/
+hugo -d poteat.github.io/
 
-cd ../poteat.github.io
+cd poteat.github.io
 git commit -am "website upload (`date`)"
 git push
-cd ../personal-web
+cd ..

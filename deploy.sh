c30abb6a31b244d280d32457e1e5b692d6afdc27
Q <poteat@users.noreply.github.com>
2019-02-01 21:06:32 -0500

    public deploy script order change

--- deploy.sh
+++ deploy.sh
@@ -9,12 +9,6 @@ echo -e "\033[0;32mBuilding project...\033[0m"
 
 hugo -t mainroad
 
-echo -e "\033[0;32mDeploying updates to source repo...\033[0m"
-
-git add .
-git commit -m "$1"
-git push origin master
-
 echo -e "\033[0;32mDeploying updates to public repo...\033[0m"
 
 cd public
@@ -22,5 +16,10 @@ git add .
 msg="rebuilding site `date`"
 git commit -m "$msg"
 git push origin master
-
 cd ..
+
+echo -e "\033[0;32mDeploying updates to source repo...\033[0m"
+
+git add .
+git commit -m "$1"
+git push origin master

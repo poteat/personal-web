5fb1c05c2a1cc38b4fd7f3a15f1ccfff0b06633f
poteat <mpoteat.o@gmail.com>
2021-09-03 23:56:24 -0700

    Update website

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,10 @@
 hugo -t mainroad -d poteat.github.io
 cd poteat.github.io
 git pull origin HEAD:master
+git add .
 git commit . -m "Update website"
 git push origin HEAD:master
 cd ..
+git add .
 git commit . -m "Update website"
 git push origin HEAD:master
